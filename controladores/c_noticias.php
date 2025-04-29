4240dd5968e920b3f89e36781cc47e9afb5d923b
Yuls1982 <YULIET_0010@HOTMAIL.COM>
2025-04-29 23:38:37 +0200

    Delete controladores directory

--- controladores/c_noticias.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'db_con.php';
-
-function obtener_noticias() {
-    global $pdo;
-    $stmt = $pdo->query("SELECT noticias.*, users_data.nombre, users_data.apellidos FROM noticias INNER JOIN users_data ON noticias.idUser = users_data.idUser ORDER BY fecha DESC");
-    return $stmt->fetchAll(PDO::FETCH_ASSOC);
-}
